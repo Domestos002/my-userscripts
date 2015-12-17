ae1d6cdd5edc3c245878233dffbc32a330703187
李方舟 <lifangzhou@meituan.com>
2015-12-17 19:21:54 +0800

    add learn-c-shortcuts

---
+++ learn-c-shortcuts.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Learn C Shortcuts
+// @namespace    http://riophae.com/
+// @version      0.1
+// @description  给 Learn C 网站的按钮添加快捷键
+// @author       Riophae Lee
+// @match        http://www.learn-c.org/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+
+(function () {
+  'use strict';
+
+  var __DEBUG__ = true;
+
+  var $ = document.querySelector.bind(document);
+
+  function log() {
+    if (!__DEBUG__) return;
+    console.log.apply(console, arguments);
+  }
+
+  var $run = $('#run-button');
+  var $reset = $('#reset-button');
+
+  var KEYCODES = {
+    ENTER: 13,
+    R: 82,
+  };
+
+  if (![$run, $reset].every(Boolean)) return;
+
+  window.addEventListener('keydown', function (event) {
+    if (
+      event.metaKey &&
+      event.keyCode === KEYCODES.ENTER
+    ) {
+      event.preventDefault();
+      $run.click();
+      log('Run...');
+    }
+
+    else if (
+      event.metaKey &&
+      event.ctrlKey &&
+      event.keyCode === KEYCODES.R
+    ) {
+      event.preventDefault();
+      $reset.click();
+      log('Reset...');
+    }
+  });
+
+  log('Learn C Shortcuts have been setup.');
+})(this);
