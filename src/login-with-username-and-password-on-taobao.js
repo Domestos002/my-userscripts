3970b38f7e01865a61bc5bedc7e1a7d7e3624608
Riophae Lee <riophaelee@gmail.com>
2016-08-25 10:48:53 +0800

    add: login with username & password on taobao

---
+++ src/login-with-username-and-password-on-taobao.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Log-in with username & password on Taobao
+// @namespace    http://riophae.com/
+// @version      0.1.0
+// @description  在淘宝登录页面，自动切换到使用用户名、密码登录
+// @author       riophae
+// @match        https://login.taobao.com/member/login.jhtml*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var $ = document.querySelector.bind(document);
+
+  var waitFor = function (checker, worker) {
+    if (checker()) {
+      worker();
+    } else {
+      setTimeout(function () {
+        waitFor(checker, worker);
+      }, 16);
+    }
+  };
+
+  waitFor(function () {
+    return $('.login-box.no-longlogin.module-quick');
+  }, function () {
+    $('#J_Quick2Static').click();
+  });
+})();
