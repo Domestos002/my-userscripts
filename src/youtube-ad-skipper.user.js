4da632e935ad822bcc7d26dfc8dff668da9d312d
Fangzhou Li <riophaelee@gmail.com>
2018-08-05 04:07:03 +0800

    add youtube ad skipper

---
+++ src/youtube-ad-skipper.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         YouTube Ad Skipper
+// @namespace    http://riophae.com/
+// @version      0.1
+// @description  Automatically skip YouTube ads.
+// @author       Riophae Lee
+// @match        https://www.youtube.com/*
+// @run-at       document-idle
+// ==/UserScript==
+
+const $ = document.querySelector.bind(document)
+
+function skipAd() {
+  const $skipBtn = $('.videoAdUiSkipButton')
+  if ($skipBtn) $skipBtn.click()
+}
+
+function closeAdBanner() {
+  const $closeBtn = $('.adDisplay .close-padding')
+  if ($closeBtn) $closeBtn.click()
+}
+
+setInterval(() => {
+  skipAd()
+  closeAdBanner()
+}, 200)
