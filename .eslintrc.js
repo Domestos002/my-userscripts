f293d90c452f9dd0a3ae78bb26f9e19a6abc1f99
Fangzhou Li <riophaelee@gmail.com>
2020-05-10 17:38:51 +0800

    fix compatibility issue with Greasemonkey 4.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: 'riophae',
+
+  env: {
+    greasemonkey: true,
+  },
+
   rules: {
+    'unicorn/prefer-node-append': 0,
     'func-names': 0,
     'no-console': 0,
   },
